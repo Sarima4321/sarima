c00ec0789cab2d6fbcb3687198bcc9ff5cbff6f6
jithin <jithin9859@gmail.com>
2024-02-19 17:06:52 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,66 @@
+//let x=10;
+//console.log(x);
+//{
+    //let x=50;
+    //console.log(x);
+//}
+//a=true;
+//console.log(a);
+//console.log(typeof(a));
+// tepm=20;
+// if(temp>30)
+// {
+//     console.log("its a hot day");
+// }
+// else if(temp<=20 &&temp>20)
+// {
+//     console.log("its a cold day");
+//}
+//else{
+    //console.log("pleasent day");
+//}
+// let i=1;
+// while(i<15)
+// {
+//     console.log(i)
+//     if(i%2==0)
+//     {
+//         console.log("even");
+
+//     }
+//     else{
+//         console.log("odd");
+//     }
+//     i++;
+// }
+// do{
+//     console.log(i)
+//     i++;
+//}
+//while(i<5)
+// 
+// 
+// function add(a,b)
+// {
+//     return a+b;
+// }
+// console.log(add(2,3));
+// let person=[{
+//     name:"sarima",
+//     age:20,
+//     city:"calicut"
+
+// },
+
+// {
+// name:"arun",
+// age:22,
+// city:"kannur"}];
+// console.log(person[0].name);
+// console.log(person[1].age.city);
+// var add(a,b)=>{
+//     var sum=a+b;
+// console.log(sum);
+// console.log("sum is $(sum)");
+// }
+// add(40,50);
